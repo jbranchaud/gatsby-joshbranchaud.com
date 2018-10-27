c09abdce591ea0bbefdf5cf69faa12a9addcc401
jbranchaud <jbranchaud@gmail.com>
2018-10-27 09:55:01 -0500

    Add styled components to the Footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,17 @@ module.exports = {
   siteMetadata: {
     title: 'Josh Branchaud',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-mdx'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-mdx`,
+      options: {
+        extensions: ['.mdx', '.md'],
+        defaultLayouts: {
+          default: require.resolve('./src/components/Layout.js'),
+        },
+      },
+    },
+    'gatsby-plugin-styled-components',
+  ],
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,36 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const FooterIconsWrapper = styled.div`
+  font-size: 2rem;
+  display: flex;
+`;
+
+const FooterIcon = styled.a`margin: 0.5rem;`;
+
+const Icon = styled.span`
+  :hover {
+    text-decoration: none;
+    color: #ffffff88;
+  }
+`;
+
 const Footer = () => {
   return (
     <section id="footer">
       <div className="inner">
-        <div className="footer-icons">
-          <a href="https://twitter.com/jbrancha">
-            <span className="icon fab fa-twitter" />
-          </a>
-          <a href="https://github.com/jbranchaud">
-            <span className="icon fab fa-github" />
-          </a>
-          <a href="https://www.instagram.com/jbranchaud">
-            <span className="icon fab fa-instagram" />
-          </a>
-        </div>
+        <FooterIconsWrapper>
+          <FooterIcon href="https://twitter.com/jbrancha">
+            <Icon className="icon fab fa-twitter" />
+          </FooterIcon>
+          <FooterIcon href="https://github.com/jbranchaud">
+            <Icon className="icon fab fa-github" />
+          </FooterIcon>
+          <FooterIcon href="https://www.instagram.com/jbranchaud">
+            <Icon className="icon fab fa-instagram" />
+          </FooterIcon>
+        </FooterIconsWrapper>
         <div className="copyright">
           <div>© 2018 Josh Branchaud</div>
           <div>
